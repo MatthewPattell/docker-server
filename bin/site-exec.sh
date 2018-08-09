62f6efb1aa6dcd08369990919b1be39cf9b7bcca
Mikhail Yarmoluk <m.yarmoluk@kakadu.bz>
2018-08-09 11:55:27 +0300

    fix docker compose helper exec command

--- bin/site-exec.sh
+++ bin/site-exec.sh
@@ -18,9 +18,9 @@ if [ "$2" = "sh" ] || [ "$2" = "/bin/sh" ]; then
 fi
 
 if [ "$COMMAND" != "" ]; then
-    COMMAND="-c $COMMAND"
+    # @TODO_ tty composer not working :( https://github.com/symfony/symfony/issues/17010
+    # https://github.com/composer/composer/issues/5856
+    docker-compose ${SERVICES} exec $SERVICE $DEFAULT_SHELL -c "$COMMAND"
+else
+    docker-compose ${SERVICES} exec $SERVICE $DEFAULT_SHELL
 fi
-
-# @TODO_ tty composer not working :( https://github.com/symfony/symfony/issues/17010
-# https://github.com/composer/composer/issues/5856
-docker-compose ${SERVICES} exec $SERVICE $DEFAULT_SHELL $COMMAND
