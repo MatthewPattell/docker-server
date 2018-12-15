a12094961bc8573d2e90c935779e3cf7b88f6357
konstantin <t.kanstantsin@gmail.com>
2018-12-15 13:55:28 +0300

    ngixn - replace perl with bash

--- docker/scripts/init-nginx.sh
+++ docker/scripts/init-nginx.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -o errexit # exit when a command fails.
+set -o pipefail # catch error in pipe operator
+set -o nounset # exit when tries to use undeclared variables.
+# set -o xtrace # debug tool
 
 # RUN ON NGINX CONTAINER
 
@@ -10,15 +15,17 @@ set +a
 ENVIRONMENT=$(echo "$PROJECT_ENVIRONMENT" | tr '[:upper:]' '[:lower:]')
 
 # find patterns on config files: <tag-name>search-string</tag-name>
-# Example: findPattern "pattern-name" "string search"
+# Example: findPattern "pattern-name" "<pattern-name>string search</pattern-name>"
 function findPattern {
-   export PERL_ARGUMENT1="$@"
-   echo $(perl -e 'if ($ENV{'PERL_ARGUMENT1'} =~ /<$ARGV[0]>(.*)<\/$ARGV[0]>/) {print $1;};' -- "$1")
+    local PATTERN=$1
+    local TEXT=$2
+
+    echo $(echo ${TEXT} | sed -n "s:.*<${PATTERN}>\(.*\)</${PATTERN}>.*:\1:p")
 }
 
 # get redis ip by host
-if [ ! -z "$REDIS_HOST" ]; then
-    export REDIS_IP=$(getent hosts "$REDIS_HOST" | cut -f 1 -d " ")
+if [[ ! -z "$REDIS_HOST" ]]; then
+    export REDIS_IP=$(getent hosts "$REDIS_HOST" | cut -d" " -f1)
 fi
 
 # COPY nginx.conf
@@ -27,82 +34,82 @@ NGINX_TEMPLATE_CODE="${NGINX_TEMPLATE_CODE//\$ENVIRONMENT/$PROJECT_ENVIRONMENT}"
 echo "${NGINX_TEMPLATE_CODE}" > /etc/nginx/nginx.conf
 
 # delete previous dynamic configs
-find /etc/nginx/conf-dynamic.d -name "*.conf" -type f -delete
+find /etc/nginx/conf-dynamic.d/ -name "*.conf" -type f -delete
 
 # create dynamic nginx configs
-for COMMON_TEMPLATE in ${PACKAGE_DOCKER_FOLDER_CONTAINER}/nginx/conf-dynamic.d/*.conf; do
-    COMMON_DYNAMIC=/etc/nginx/conf-dynamic.d/$(basename $COMMON_TEMPLATE)
+for TEMPLATE_NAME in ${PACKAGE_DOCKER_FOLDER_CONTAINER}/nginx/conf-dynamic.d/*.conf; do
+    TEMPLATE_PATH=/etc/nginx/conf-dynamic.d/$(basename ${TEMPLATE_NAME})
 
-    # get templace
-    TEMPLATE_CODE=$(cat $COMMON_TEMPLATE)
+    # get template content
+    BASE_TEMPLATE_CODE=$(cat ${TEMPLATE_NAME})
 
     # copy template
-    TEMPLATE_PATH=$(findPattern "template" $TEMPLATE_CODE)
+    TEMPLATE_PATH=$(findPattern "template" ${BASE_TEMPLATE_CODE})
 
-    if [ ! -z $TEMPLATE_PATH ]; then
-        if [ ! -f $TEMPLATE_PATH ]; then
-            TEMPLATE_PATH="${PACKAGE_DOCKER_FOLDER_CONTAINER}/nginx/$TEMPLATE_PATH"
+    if [[ ! -z ${TEMPLATE_PATH} ]]; then
+        if [[ ! -f ${TEMPLATE_PATH} ]]; then
+            TEMPLATE_PATH="${PACKAGE_DOCKER_FOLDER_CONTAINER}/nginx/${TEMPLATE_PATH}"
         fi
 
         # get template code without comments (if "copy template" directive exist)
-        TEMPLATE_CODE=$(echo -e "$TEMPLATE_CODE \n\n$(grep -o '^[^#]*' $TEMPLATE_PATH)")
+        BASE_TEMPLATE_CODE=$(echo -e "$BASE_TEMPLATE_CODE \n\n$(grep -o '^[^#]*' ${TEMPLATE_PATH})")
     fi
-
-    # allow domain
-    ONLY_DOMAINS=$(findPattern "domains-include" $TEMPLATE_CODE)
-    ONLY_DOMAINS=$(echo $(perl -e 'print $ENV{$ARGV[0]}' -- $ONLY_DOMAINS))
-
-    # root path
-    REPLACE_ROOT=$(findPattern "root-path" $TEMPLATE_CODE)
 
     # Clean file
-    echo "" > $COMMON_DYNAMIC
+    echo "" > ${TEMPLATE_PATH}
+
+    # allow domain
+    ONLY_DOMAINS=$(findPattern "domains-include" ${BASE_TEMPLATE_CODE})
+    # TODO: replace with bash.
+    ONLY_DOMAINS=${!ONLY_DOMAINS}
+
+    # root path
+    REPLACE_ROOT=$(findPattern "root-path" ${BASE_TEMPLATE_CODE})
 
     # if empty domains, remove config
-    if [ -z "$ONLY_DOMAINS" ]; then
-        rm $COMMON_DYNAMIC
+    if [[ -z "$ONLY_DOMAINS" ]]; then
+        rm ${TEMPLATE_PATH}
+        continue
     fi
 
-    for DOMAIN in $ONLY_DOMAINS; do
+    for DOMAIN in ${ONLY_DOMAINS}; do
         # TODO: TOPDOMAIN and C_DOMAIN used only in certificates pattern. Replace them with $CERTIFICATE_DOMAIN variable.
-        TOPDOMAIN=$(perl -e 'if ($ARGV[0] =~ /^((?<subdomain>[^\.]*)\.)?(?<domain>[^\.]*)\.(?<topdomain>((com\.ua)|.*))$/) {print $+{topdomain};};' -- "$DOMAIN")
-        C_DOMAIN=$(perl -e 'if ($ARGV[0] =~ /^((?<subdomain>[^\.]*)\.)?(?<domain>[^\.]*)\.(?<topdomain>(com\.)?.*)$/) {print $+{domain};};' -- "$DOMAIN")
-        TEMPLATE=$TEMPLATE_CODE
-        TEMPLATE="${TEMPLATE//\$COMMON_DOMAIN/$DOMAIN}"
-        TEMPLATE="${TEMPLATE//\$ENVIRONMENT/$ENVIRONMENT}"
-        TEMPLATE="${TEMPLATE//\$TOPDOMAIN/$TOPDOMAIN}"
-        TEMPLATE="${TEMPLATE//\$C_DOMAIN/$C_DOMAIN}"
-        TEMPLATE="${TEMPLATE//\$PARSED_DOMAINS/$ONLY_DOMAINS}"
-        TEMPLATE="${TEMPLATE//\$ROOT_PATH/$REPLACE_ROOT}"
+        # TODO: replace with bash.
+        TOPDOMAIN=$(echo ${DOMAIN} | sed -n "s:(([^\.]*)\.)?([^\.]*)\.([^\.]*):\4:p")
+        C_DOMAIN=$(echo ${DOMAIN} | sed -n "s:(([^\.]*)\.)?([^\.]*)\.([^\.]*):\3:p")
+        TEMPLATE_CODE=${BASE_TEMPLATE_CODE}
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$COMMON_DOMAIN/$DOMAIN}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$ENVIRONMENT/$ENVIRONMENT}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$TOPDOMAIN/$TOPDOMAIN}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$C_DOMAIN/$C_DOMAIN}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$PARSED_DOMAINS/$ONLY_DOMAINS}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$ROOT_PATH/$REPLACE_ROOT}"
 
-        # TODO: rewrite get-certificates with sh and this part of script.
-        for each in $(cat "${ENV_PATH}" | awk -F= '/^SSL_DOMAINS\[[0-9]\]/ {print $2}')
-        do
-            LIST_CERTIFICATE_DOMAINS=$(echo "$each" | cut -d ':' -f 2)
-            MAIN_CERTIFICATE=$(echo "$LIST_CERTIFICATE_DOMAINS" | cut -d ' ' -f 1)
-            for CERTIFICATE_DOMAIN in $LIST_CERTIFICATE_DOMAINS; do
-                if [ "$CERTIFICATE_DOMAIN" = "$DOMAIN" ]; then
-                    TEMPLATE="${TEMPLATE//\$CERTIFICATE_DOMAIN/$MAIN_CERTIFICATE}"
+        for i in ${!SSL_DOMAINS[*]}; do
+            LIST_CERTIFICATE_DOMAINS=$(echo "${SSL_DOMAINS[$i]}" | cut -d ':' -f 2)
+            MAIN_CERTIFICATE=$(echo ${LIST_CERTIFICATE_DOMAINS} | cut -d ' ' -f 1)
+            for CERTIFICATE_DOMAIN in ${LIST_CERTIFICATE_DOMAINS}; do
+                if [[ "${CERTIFICATE_DOMAIN}" = "${DOMAIN}" ]]; then
+                    TEMPLATE_CODE="${TEMPLATE_CODE//\$CERTIFICATE_DOMAIN/$MAIN_CERTIFICATE}"
                 fi
             done
         done
 
-        SSL_DERICTIVE=""
-
         # find external certificate domain
-        CERTIFICATE_DOMAIN=$(findPattern "certificate-domain" $TEMPLATE)
-
-        # find server_name for check certificate
-        if [ -z $CERTIFICATE_DOMAIN ]; then
-            CERTIFICATE_DOMAIN=$(perl -e '$str = $ARGV[0];if ($str =~ /server_name([A-z.0-9]*);/) {print $1;};' -- $(echo $TEMPLATE | sed 's/ //g'))
+        CERTIFICATE_DOMAIN=$(findPattern "certificate-domain" ${TEMPLATE_CODE})
+        # or use current server_name (DOMAIN)
+        if [[ -z ${CERTIFICATE_DOMAIN} ]]; then
+            # TODO: replace with bash.
+            CERTIFICATE_DOMAIN=DOMAIN
         fi
 
-        if [ $CERTIFICATE_DOMAIN != "" ]; then
+        SSL_DIRECTIVE=""
+        if [[ "${CERTIFICATE_DOMAIN}" != "" ]]; then
             SSL_KEY=/etc/letsencrypt/live/${CERTIFICATE_DOMAIN}/privkey.pem;
             SSL_CERT=/etc/letsencrypt/live/${CERTIFICATE_DOMAIN}/fullchain.pem;
 
-            if [ -f $SSL_KEY ] && [ -f $SSL_CERT ]; then
-                SSL_DERICTIVE="
+            if [[ -f ${SSL_KEY} ]] && [[ -f ${SSL_CERT} ]]; then
+                SSL_DIRECTIVE="
     ssl_certificate             ${SSL_CERT};
     ssl_certificate_key         ${SSL_KEY};
     ssl_trusted_certificate     ${SSL_CERT};
@@ -111,9 +118,9 @@ for COMMON_TEMPLATE in ${PACKAGE_DOCKER_FOLDER_CONTAINER}/nginx/conf-dynamic.d/*
             fi
         fi
 
-        TEMPLATE="${TEMPLATE//\$SSL_INCLUDE/$SSL_DERICTIVE}"
+        TEMPLATE_CODE="${TEMPLATE_CODE//\$SSL_INCLUDE/$SSL_DIRECTIVE}"
 
-        echo "${TEMPLATE}" >> $COMMON_DYNAMIC
+        echo "${TEMPLATE_CODE}" >> ${TEMPLATE_PATH}
     done
 done
 
