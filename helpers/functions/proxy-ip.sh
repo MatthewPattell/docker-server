caa04446cf87f1879df33c2abd674f4571ffe331
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2019-12-30 11:23:34 +0300

    Fix freeze

--- helpers/functions/proxy-ip.sh
+++ helpers/functions/proxy-ip.sh
@@ -2,7 +2,7 @@
 
 function nginxProxyIp() {
     if [ -z "$HOST_ETC_HOSTS_IP" ]; then
-         HOST_ETC_HOSTS_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{println .IPAddress}}{{end}}' "${PROJECT_NAME}_nginx" | head -n1 && tail -n1)
+         HOST_ETC_HOSTS_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{println .IPAddress}}{{end}}' "${PROJECT_NAME}_nginx" | head -n1)
 
          if [[ $(< $PROJECT_ENV_PATH) != "HOST_ETC_HOSTS_IP" ]]; then
             echo "HOST_ETC_HOSTS_IP=$HOST_ETC_HOSTS_IP" >> $PROJECT_ENV_PATH
